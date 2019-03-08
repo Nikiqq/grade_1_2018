8d199907b30ff8a6b17e06d6580f5d613b6cd115
nkirillov <nkirillov@team.amocrm.com>
2019-03-08 18:24:20 +0300

    Оптимизация кода на JS

--- js/main.js
+++ js/main.js
@@ -1,84 +1,36 @@
 $(function(){
-    $(".form-entity").on("submit", function(event){
-        event.preventDefault();
-        let numbers = {};
-        $(".form-entity__input").each(function(index, element) {
-            numbers[$(element).attr("name")] = element.value;
-        })
+
+    function prepare_data(className) {
+        let arrayData = {};
+        className.find(".form__input, .form__select").each(function(index, element) {
+            arrayData[$(element).attr("name")] = element.value;
+        });
+        return arrayData;
+    }
+
+    function ajaxQuery(data, elem) {
         $.ajax({
             type: "POST",
             url: "scripts/add_entity.php",
-            data: numbers,
+            data: data,
             success: function(result){
-                console.log(result);
+                if(elem) {
+                    elem.val(result);
+                }
+                else {
+                    console.log(result);
+                }
             }
         });
-    })
+    }
 
-    $(".form-text").on("submit", function(event){
+    $(".form").on("submit", function(event){
         event.preventDefault();
-        let numbers_text_field = {};
-        $(".form-text__input").each(function(index, element) {
-            numbers_text_field[$(element).attr("name")] = element.value;
-        })
-        $.ajax({
-            type: "POST",
-            url: "scripts/add_entity.php",
-            data: numbers_text_field,
-            success: function(result){
-                console.log(result);
-            }
-        });
-    })
-
-    $(".form-note").on("submit", function(event){
-        event.preventDefault();
-        let add_note_field = {};
-        $(".form-note__input").each(function(index, element) {
-            add_note_field[$(element).attr("name")] = element.value;
-        })
-        add_note_field["add_note_field_list"] = $(".form-note__select").val();
-        $.ajax({
-            type: "POST",
-            url: "scripts/add_entity.php",
-            data: add_note_field,
-            success: function(result){
-                console.log(result);
-            }
-        });
-    })
-
-    $(".form-task").on("submit", function(event){
-        event.preventDefault();
-        let add_task_field = {};
-        $(".form-task__input").each(function(index, element) {
-            add_task_field[$(element).attr("name")] = element.value;
-        })
-
-        $.ajax({
-            type: "POST",
-            url: "scripts/add_entity.php",
-            data: add_task_field,
-            success: function(result){
-                $('input[name="add_task_end_id"]').val(result);
-            }
-        });
-    })
-
-    $(".form-task-end").on("submit", function(event){
-        event.preventDefault();
-        let close_task_field = {};
-        $(".form-task-end__input").each(function(index, element) {
-            close_task_field[$(element).attr("name")] = element.value;
-        })
-
-        $.ajax({
-            type: "POST",
-            url: "scripts/add_entity.php",
-            data: close_task_field,
-            success: function(result){
-                console.log(result);
-            }
-        });
-    })
-})
+        let data = prepare_data($(this)),
+            elem = false;
+        if($(this).hasClass("form-task")) {
+            elem = $('input[name="task_end_id"]');
+        }
+        ajaxQuery(data, elem);
+    });
+});
